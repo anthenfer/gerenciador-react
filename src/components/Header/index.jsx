5a91f84c212a3cdba46cb5fb4fa4415f2834131e
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-04-19 11:18:56 -0300

    Criando primeiro repositório React

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header(){
+    return(
+        <header>
+            <a href="">Usuários</a>
+            <a href="">Cadastrar Usuário</a>
+            <a href="">Cadastrar Endereço</a>
+        </header>
+    );
+};
+
+export default Header;
